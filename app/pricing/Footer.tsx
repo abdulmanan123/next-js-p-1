20d0f0d53599f533be1e4b76730db333a8d08627
Abdul Manan <abdulmanan@pitb.gov.pk>
2023-01-31 12:12:36 +0500

    pricing page

--- app/pricing/Footer.tsx
+++ app/pricing/Footer.tsx
@@ -6,21 +6,21 @@ import Icon3 from './Icon3'
 
 export default function Body() {
   return (
-    <Box maxW="800px" m="auto" mt="70px" px="50px" >
+    <Box maxW="900px" m="auto" mt="70px" px="50px" >
       <Flex direction={{ base: "column", md: "column", lg: "row" }}>
           <HStack mb="20px">
             <Icon as={Icon1} ></Icon>
-            <Text fontWeight="bold" pl="20px">30 days money back Guarantee</Text>
+            <Text fontWeight="bold" px="10px">30 days money back Guarantee</Text>
           </HStack>
 
           <HStack mb="20px">
             <Icon as={Icon2} ></Icon>
-            <Text fontWeight="bold" pl="20px">No setup fees 100% hassle-free</Text>
+            <Text fontWeight="bold" px="10px">No setup fees 100% hassle-free</Text>
           </HStack>
 
           <HStack mb="20px">
             <Icon as={Icon3} ></Icon>
-            <Text fontWeight="bold" pl="20px">No monthly subscription Pay once and for all</Text>
+            <Text fontWeight="bold" px="10px">No monthly subscription Pay once and for all</Text>
           </HStack>
       </Flex>
       
--- components/Todos.tsx
+++ components/Todos.tsx
@@ -1,23 +1,23 @@
 
 
-const getTodoList = async () => {
-  let todos = await fetch("http://jsonplaceholder.typicode.com/todos?userId=1");
-  return todos.json();
-}
+// const getTodoList = async () => {
+//   let todos = await fetch("http://jsonplaceholder.typicode.com/todos?userId=1");
+//   return todos.json();
+// }
 
-export default async function TodoList() {
-  const { todos } = await getTodoList();
-  console.log(todos);
+// export default async function TodoList() {
+//   const { todos } = await getTodoList();
+//   console.log(todos);
 
-  return (
-    <div>
-      {
-        todos.map(t) => {
-          return {
-            <h2>{t.name}</h2>
-          }
-        }
-      }
-    </div>
-  )
-} 
+//   return (
+//     <div>
+//       {
+//         todos.map(t) => {
+//           return {
+//             <h2>{t.name}</h2>
+//           }
+//         }
+//       }
+//     </div>
+//   )
+// } 
