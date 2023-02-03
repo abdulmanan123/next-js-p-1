50435b0d6c075d96bf815179447121ba55c14e18
Abdul Manan <abdulmanan@pitb.gov.pk>
2023-02-03 16:07:01 +0500

    todo app

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { border, Button } from "@chakra-ui/react";
+import { border, Box, Button, Flex, Input } from "@chakra-ui/react";
 import React, { useState } from "react";
 
 export default function Todos() {
@@ -54,48 +54,71 @@ export default function Todos() {
 
   return (
     <>
-      <input
-        type="text"
-        style={{ border: "1px solid black" }}
-        value={todo}
-        onChange={(e) => {
-          setTodo(e.target.value);
-        }}
-      />
-      <Button onClick={addTodo}>Add</Button>
+      <Box
+        maxW="450px"
+        mt="30px"
+        mx={{ base: "20px", lg: "auto" }}
+        borderRadius="12px"
+        boxShadow="0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04)"
+      >
+        <Flex direction={{ base: "column", md: "column", lg: "row" }}>
+          <Input
+            type="text"
+            style={{ border: "1px solid black" }}
+            value={todo}
+            onChange={(e) => {
+              setTodo(e.target.value);
+            }}
+          />
+          <Button onClick={addTodo}>Add</Button>
+        </Flex>
 
-      <ul>
-        {todos.map((todo) => {
-          return (
-            <li key={todo.text}>
-              <input
-                type="checkbox"
-                checked={todo.completed}
-                onClick={() => {
-                  checkboxHandler(todo);
-                }}
-              />
-              <b
+        <ul
+          style={{
+            marginTop: "10px",
+            marginLeft: "10px",
+            listStyle: "none",
+          }}
+        >
+          {todos.map((todo) => {
+            return (
+              <li
+                key={todo.text}
                 style={{
-                  color: todo.completed ? "green" : "orange",
-                  fontStyle: todo.completed ? "oblique" : "",
+                  marginLeft: "10px",
+                  marginBottom: "7px",
                 }}
               >
-                {" "}
-                {todo.text}
-              </b>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onClick={() => {
+                    checkboxHandler(todo);
+                  }}
+                />
+                <b
+                  style={{
+                    color: todo.completed ? "green" : "orange",
+                    fontStyle: todo.completed ? "oblique" : "",
+                    marginLeft: "10px",
+                  }}
+                >
+                  {todo.text}
+                </b>
 
-              <Button
-                onClick={() => {
-                  deleteTodo(todo);
-                }}
-              >
-                Delete
-              </Button>
-            </li>
-          );
-        })}
-      </ul>
+                <Button
+                  ml="5"
+                  onClick={() => {
+                    deleteTodo(todo);
+                  }}
+                >
+                  Delete
+                </Button>
+              </li>
+            );
+          })}
+        </ul>
+      </Box>
     </>
   );
 }
